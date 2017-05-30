94bd4a783e7c59b16c075e2cb5e2db3a7f8216e9
huangcheng <huangcheng@wead.mobi>
2017-05-30 23:31:11 +0800

    save

--- src/views/open/openFooter.vue
+++ src/views/open/openFooter.vue
@@ -1,11 +1,60 @@
 <template>
-
+  <div class="table-footer">
+    <ul>
+      <li class="table-prev">
+        <a href="javascript:void(0);" @click="pushPrev">
+          <i class="fa fa-chevron-circle-left"></i>
+        </a>
+      </li>
+      <li class="table-page">
+        <input type="number" :value="page" @change="pushPage"><span>/{{totalPage}}</span>
+      </li>
+      <li class="table-next">
+        <a href="javascript:void(0);" @click="pushNext">
+          <i class="fa fa-chevron-circle-right"></i>
+        </a>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
-export default {
-  name: 'OpenFooter'
-}
+  import { mapGetters, mapActions } from 'vuex'
+  export default {
+    name: 'OpenFooter',
+    computed: {
+      ...mapGetters({
+        page: 'openList/page',
+        totalPage: 'openList/totalPage'
+      })
+    },
+    methods: {
+      ...mapActions({
+        changePage: 'openList/changePage'
+      }),
+      pushPrev() {
+        let self = this
+        if(self.page > 1) {
+          this.changePage(--self.page)
+        }
+      },
+      pushNext() {
+        let self = this
+        if(self.page < self.totalPage) {
+          this.changePage(++self.page)
+        }
+      },
+      pushPage(e) {
+        let self = this
+        let value = e.target.value
+        if(value >= 1 && value <= self.totalPage) {
+          this.changePage(value)
+        }else {
+          e.target.value = self.page
+        }
+      }
+    }
+  }
 </script>
 
 <style>
