bfe5990adf6e4e8be82e3be8c11f6d68a2fee764
huangcheng <huangcheng@wead.mobi>
2017-06-08 19:47:18 +0800

    save

--- src/views/private/privateHeader.vue
+++ src/views/private/privateHeader.vue
@@ -1,22 +1,24 @@
 <template>
   <div class="table-header">
-    <ul v-if="choosed.length !== 0" class="table-operation">
-      <li v-if="role===3||role===2||role===1||role===0">
-        <i class="fa fa-mail-forward"></i>
-        <span>领取</span>
-      </li>
-      <li v-if="role===2||role===1||role===0">
-        <i class="fa fa-anchor"></i>
-        <span>分配</span>
-      </li>
-      <li v-if="role===0">
-        <i class="fa fa-trash"></i>
-        <span>删除</span>
-      </li>
-    </ul>
-    <span v-if="choosed.length === 0">私人池</span>
-    <div class="btn-green-mid">
-      新建客户
+    <div class="table-opera">
+      <ul v-if="choosed.length !== 0" class="table-operation">
+        <li v-if="role===3||role===2||role===1||role===0">
+          <i class="fa fa-mail-forward"></i>
+          <span>领取</span>
+        </li>
+        <li v-if="role===2||role===1||role===0">
+          <i class="fa fa-anchor"></i>
+          <span>分配</span>
+        </li>
+        <li v-if="role===0">
+          <i class="fa fa-trash"></i>
+          <span>删除</span>
+        </li>
+      </ul>
+      <span v-if="choosed.length === 0">私人池</span>
+      <div class="btn-green-mid">
+        新建客户
+      </div>
     </div>
     <div class="table-search">
       <b-autocomplete
